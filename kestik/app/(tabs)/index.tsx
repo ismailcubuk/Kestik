bd437405241f49933828700e81d76dce9a88243c
ismailcubuk <iismailcubuk@gmail.com>
2024-11-03 10:57:46 +0300

    tailwind added

--- kestik/app/(tabs)/index.tsx
+++ kestik/app/(tabs)/index.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>START!</Text>
+    <View >
+      <Text className='bg-red-500'>START!</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
 
 export default HomeScreen;
